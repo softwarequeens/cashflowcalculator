71d9b22a03502b79fed18039553a13d42420c3f2
zgao <ziyang.gao@miacanalytics.com>
2017-12-07 12:26:32 -0500

    Working Commit

---
+++ CashflowCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashflowCalculator
+{
+    class Calculator
+    {
+        private int amount, duration, r, numOfMonth, remainBal;
+
+        public static int MonthlyPayment(int amount, int duration, int r)
+        {
+            int MonthlyPay = amount * (r / 1200) / (1 - (1 + r / 1200) ^ (-duration*12));
+            return MonthlyPay;
+        }
+        public static int InterestCalc(int amount, int r, int remainBal)
+        {
+            int InterestPayment = remainBal * r / 1200;
+            return InterestPayment;
+        }
+        public static int PrincipalPay(int MonthlyPay, int InterestPayment)
+        {
+            int PrincipalPay = MonthlyPay - InterestPayment;
+            return PrincipalPay;
+        }
+
+    }
+}
--- CashflowCalculator/Program.cs
+++ CashflowCalculator/Program.cs
@@ -10,6 +10,38 @@ namespace CashflowCalculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter in your loan amount: ");
+            string amount1 = Console.ReadLine();
+            float amount = Int32.Parse(amount1);
+            Console.WriteLine(string.Format("You entered {0}", amount));
+
+            Console.Write("Please enter in your loan duration: ");
+            string duration1 = Console.ReadLine();
+            int duration = Int32.Parse(duration1);
+            Console.WriteLine(string.Format("You entered {0}", duration));
+
+            Console.Write("Please enter in your interest rate: ");
+            string r1 = Console.ReadLine();
+            float r = float.Parse(r1);
+            Console.WriteLine(string.Format("You entered {0}", r));
+
+            int ind = 1;
+            int numOfMonth = 0;
+            int principal = 0;
+            int remainBal = amount - principal;
+            while (ind>0)
+            {
+                numOfMonth++;
+                int interestPay = Calculator.InterestCalc(amount, r, remainBal);
+                int principalPay = Calculator.PrincipalPay(remainBal,interestPay);
+                remainBal  = remainBal - interestPay;
+                Console.WriteLine(numOfMonth + " " + interestPay + " " + principalPay + " " + remainBal);
+                Console.Write("Would you want to calculate for next month? Enter Yes or No");
+                string conti = Console.ReadLine();
+                if (conti.Equals("Yes")) {ind = 1;}
+                else { ind = 0; }
+            }
+
         }
     }
 }
